beb7287536a9c278b1abce755765598a0d1455c8
Zabdin19 <zainjee331@gmail.com>
2026-06-23 16:15:45 +0500

    Initial commit: exported fixtures

---
+++ ecommerce/ecommerce/doctype/product_subcategory/test_product_subcategory.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Zain-ul-Abdin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductSubcategory(FrappeTestCase):
+	pass
